ced1f8614f98019fb38cac059d7f1a552deee706
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-08-10 18:57:22 +0600

    updated profile and finished basket database

--- app/src/main/java/com/example/shop/Product.java
+++ app/src/main/java/com/example/shop/Product.java
@@ -1,80 +1,46 @@
 package com.example.shop;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Parcelable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.Constraints;
-
-import com.squareup.picasso.Picasso;
-
-import java.io.File;
 import java.io.Serializable;
 
-public class Product {
+public class Product implements Serializable {
 
     private String name;
     private int price;
     private String size;
     private String color;
     private String resourceDrawable;
-    private LayoutInflater layoutInflater;
-    private int sizeScreen;
-    public int imageWidth;
-    public Product(String name, int price, String size, String color, String resourceDrawable, LayoutInflater layoutInflater,
-                   int sizeScreen) {
+    private String vendorCode;
+    public Product() {}
+
+    public Product(String name, int price, String size, String color, String resourceDrawable, String vendorCode) {
 
         this.name             =             name;
         this.price            =            price;
         this.size             =             size;
         this.color            =            color;
         this.resourceDrawable = resourceDrawable;
-        this.layoutInflater   =   layoutInflater;
-        this.sizeScreen       =       sizeScreen;
+        this.vendorCode       =       vendorCode;
     }
 
-    public Product() {}
+    public Product(Product product) {
 
-    public View CreateProduct() {
-
-        View product = this.layoutInflater.inflate(R.layout.layout_product, null, false);
-        product.setId(View.generateViewId());
-        ImageView imageView = (ImageView) product.findViewById(R.id.productImage);
-        TextView productName = (TextView)  product.findViewById(R.id.productName);
-        TextView productPrice = (TextView) product.findViewById(R.id.productPrice);
-
-        Picasso.get().load(this.resourceDrawable).into(imageView);
-        imageView.getLayoutParams().width  = this.sizeScreen / 2 - 50;
-        imageView.getLayoutParams().height = this.sizeScreen / 2 - 50;
-        this.imageWidth = imageView.getLayoutParams().width;
-
-        productName.setText(this.name);
-        productName.setTextSize(12);
-        productPrice.setText(this.price + " ₽");
-        productPrice.setTextSize(12);
-
-        return product;
+        this.name             = product.getName();
+        this.price            = product.getPrice();
+        this.size             = product.getSize();
+        this.color            = product.getColor();
+        this.resourceDrawable = product.getResourceDrawable();
     }
+
+    public void setName(String name)   {this.name = name;}
+    public void setPrice(int price) {this.price = price;}
+    public void setSize(String size) {this.size = size;}
+    public void setColor(String color) {this.color = color;}
+    public void setResourceDrawable(String resourceDrawable) {this.resourceDrawable = resourceDrawable;}
+    public void setVendorCode(String vendorCode) {this.vendorCode = vendorCode;}
     public String getName()  { return this.name;}
     public int getPrice() { return this.price;}
     public String getSize() {return this.size;}
     public String getColor() {return this.color;}
     public String getResourceDrawable() {return this.resourceDrawable;}
-    public void setName(String name) {this.name = name;}
-    public void setPrice(int price) {this.price = price;}
-    public void setSize(String size) {this.size = size;}
-    public void setColor(String color) {this.color = color;}
-    public void setResourceDrawable(String resourceDrawable) {this.resourceDrawable = resourceDrawable;}
+    public String getVendorCode() {return this.vendorCode;}
 }
--- app/src/main/java/com/example/shop/ProductBasketListAdapter.java
+++ app/src/main/java/com/example/shop/ProductBasketListAdapter.java
@@ -2,11 +2,14 @@ package com.example.shop;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import static java.security.AccessController.getContext;
+
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.telephony.mbms.MbmsErrors;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,12 +21,16 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -33,13 +40,14 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
 
     private Context context;
     private final LayoutInflater inflater;
-    private final List<ProductSave> products;
+    private ArrayList<Product> products;
     private ArrayList<Integer> amountProducts;
     private int size;
-    final int maxAmountProducts = 10;
-    SQLiteDatabase dbBasketProducts;
-    Button buyAllProductsButton;
-    public ProductBasketListAdapter(Context context, List<ProductSave> products, int size,
+    private final int maxAmountProducts = 10;
+    private SQLiteDatabase dbBasketProducts;
+    private Cursor queryBasketProducts;
+    private Button buyAllProductsButton;
+    public ProductBasketListAdapter(Context context, ArrayList<Product> products, int size,
                                     Button buyAllProductsButton) {
 
         this.context  = context;
@@ -48,9 +56,10 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
         this.size     =     size;
 
         this.amountProducts = new ArrayList<>();
-        int i = 0;
-        for(i = 0; i < this.products.size(); i++)
+        int indexProduct = 0;
+        for(indexProduct = 0; indexProduct < this.products.size(); indexProduct++)
             this.amountProducts.add(1);
+
         this.buyAllProductsButton = buyAllProductsButton;
     }
 
@@ -62,25 +71,14 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
 
     public void onBindViewHolder(ProductBasketListAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
 
-        dbBasketProducts =
-                this.context.openOrCreateDatabase("basket.db", MODE_PRIVATE, null);
-        Cursor queryBasketProducts = dbBasketProducts.rawQuery("SELECT * FROM products;",
-                null);
-
-        int i = 0;
-        while(queryBasketProducts.moveToNext()) {
-
-            amountProducts.add(i, queryBasketProducts.getInt(4));
-            i++;
-        }
-
+        InitializeDBBasketProductsSQL();
+        InitializeBasketProducts();
         buyAllProductsButton.setText("КУПИТЬ ВСЁ ЗА " + fullSumProducts() + " ₽");
 
-        ProductSave basketProduct = products.get(position);
+        Product basketProduct = products.get(position);
         queryBasketProducts.moveToPosition(position);
 
         holder.constraintBasketProduct.getLayoutParams().width = size - 70;
-
         holder.basketProductName.setText(basketProduct.getName());
         holder.basketProductSize.setText("Размер: " + basketProduct.getSize());
         holder.basketProductColor.setText("Цвет: " + basketProduct.getColor());
@@ -88,24 +86,18 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
         holder.basketOldProductPrice.setText(Html.fromHtml("<s>" + "5000₽" + "</s>"));
         holder.basketProductImage.getLayoutParams().width  = size / 3;
         holder.basketProductImage.getLayoutParams().height = size / 3;
+        holder.returnTextView.setMaxWidth(400);
         Picasso.get().load(basketProduct.getResourceDrawable()).into(holder.basketProductImage);
 
-        holder.returnTextView.setMaxWidth(400);
-
         holder.amountProductsTextView.setText(queryBasketProducts.getInt(4) + "");
+        updateFirebaseDB();
         holder.productButtonPositive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(amountProducts.get(position) < maxAmountProducts) {
-
-                    amountProducts.set(position, amountProducts.get(position) + 1);
-                    holder.amountProductsTextView.setText(amountProducts.get(position) + "");
-                    ContentValues cv = new ContentValues();
-                    cv.put("amount", amountProducts.get(position));
-                    dbBasketProducts.update("products", cv, "name=?", new String[]{basketProduct.getName()});
-                    buyAllProductsButton.setText("КУПИТЬ ВСЁ ЗА " + fullSumProducts() + " ₽");
-                }
+                if(amountProducts.get(position) < maxAmountProducts)
+                    updateAmountItem(holder, basketProduct,
+                            amountProducts.get(position) + 1, position);
             }
         });
 
@@ -113,20 +105,28 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
             @Override
             public void onClick(View v) {
 
-                if(amountProducts.get(position) > 1) {
-
-                    amountProducts.set(position, amountProducts.get(position) - 1);
-                    holder.amountProductsTextView.setText(amountProducts.get(position) + "");
-                    ContentValues cv = new ContentValues();
-                    cv.put("amount", amountProducts.get(position));
-                    dbBasketProducts.update("products", cv, "name=?", new String[]{basketProduct.getName()});
-                    buyAllProductsButton.setText("КУПИТЬ ВСЁ ЗА " + fullSumProducts() + " ₽");
-                }
+                if(amountProducts.get(position) > 1)
+                    updateAmountItem(holder, basketProduct,
+                            amountProducts.get(position) - 1, position);
             }
         });
 
-    }
+        holder.buyItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                rejectionToast(v);
+            }
+        });
+
+        buyAllProductsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                rejectionToast(v);
+            }
+        });
+    }
 
     public int getItemCount() {
 
@@ -144,6 +144,7 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
         final ImageView basketProductImage;
         final ImageButton productButtonNegative,
                           productButtonPositive;
+        final Button buyItemButton;
         final ConstraintLayout constraintBasketProduct;
 
         ViewHolder(View view) {
@@ -159,6 +160,7 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
             basketProductImage       = view.findViewById(R.id.basketProductImage);
             productButtonNegative    = view.findViewById(R.id.productButtonNegative);
             productButtonPositive    = view.findViewById(R.id.productButtonPositive);
+            buyItemButton            = view.findViewById(R.id.buyItemButton);
             constraintBasketProduct  = view.findViewById(R.id.constraintBasketProduct);
             returnTextView           = view.findViewById(R.id.returnTextView);
         }
@@ -172,5 +174,63 @@ public class ProductBasketListAdapter extends RecyclerView.Adapter<ProductBasket
             fullSum += products.get(indexProduct).getPrice() * amountProducts.get(indexProduct);
 
         return fullSum;
+    }
+    private void InitializeBasketProducts() {
+
+        int indexAmountProducts = 0;
+        while(queryBasketProducts.moveToNext()) {
+
+            amountProducts.set(indexAmountProducts, queryBasketProducts.getInt(4));
+            indexAmountProducts++;
+        }
+    }
+
+    private void InitializeDBBasketProductsSQL() {
+
+        dbBasketProducts =
+                this.context.openOrCreateDatabase("basket.db", MODE_PRIVATE, null);
+        queryBasketProducts = dbBasketProducts.rawQuery("SELECT * FROM products;",
+                null);
+    }
+    private void rejectionToast(View view) {
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+            Toast.makeText(view.getContext(), "Необходимо авторизоваться, " +
+                    "чтобы оплатить заказ.", Toast.LENGTH_LONG).show();
+    }
+    private void updateAmountItem(ViewHolder holder, Product basketProduct, int addOrRemove,
+                                  int position) {
+
+        amountProducts.set(position, addOrRemove);
+        holder.amountProductsTextView.setText(amountProducts.get(position) + "");
+        ContentValues cv = new ContentValues();
+        cv.put("amount", amountProducts.get(position));
+        dbBasketProducts.update("products", cv, "name=?", new String[]{basketProduct.getName()});
+        buyAllProductsButton.setText("КУПИТЬ ВСЁ ЗА " + fullSumProducts() + " ₽");
+        updateFirebaseDB();
+    }
+    private void updateFirebaseDB() {
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+            int indexProduct = 0;
+            for(indexProduct = 0; indexProduct < amountProducts.size(); indexProduct++) {
+
+                FirebaseDatabase.getInstance().getReference()
+                        .child("Users")
+                        .child(FirebaseAuth.getInstance().getUid())
+                        .child("Basket")
+                        .child("product-" + (indexProduct + 1))
+                        .setValue(products.get(indexProduct));
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .child("Users")
+                        .child(FirebaseAuth.getInstance().getUid())
+                        .child("Basket")
+                        .child("product-" + (indexProduct + 1))
+                        .child("amount")
+                        .setValue(amountProducts.get(indexProduct));
+            }
+        }
     }
 }
